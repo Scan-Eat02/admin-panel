68608f125740730d31b07dfceff3046a7ce47f35
Vishesh-web-dev <visheshshrivastava2002@gmail.com>
2024-11-10 00:25:34 +0530

    react query and intial routing for login setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { Suspense, lazy } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./apis";
+import Cookies from "js-cookie";
+import { USER_ACCESS_KEY } from "./utils/enums";
+
+const Login = lazy(() => import("./pages/authentication/login"));
 function App() {
-  return <>Scan And Eat</>;
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> add check here  */}
+            <Route
+              path="/"
+              element={
+                Cookies.get(USER_ACCESS_KEY.TOKEN) ? (
+                  <Navigate to="/home" replace />
+                ) : (
+                  <Navigate to="/login" replace />
+                )
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/signup" element={<Signup />} /> */}
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </Suspense>
+  );
 }
 
 export default App;
---
+++ src/apis/queryClient.ts
@@ -0,0 +1,28 @@
+import { QueryClient } from "@tanstack/react-query";
+
+const onError = (error: unknown) => {
+  console.log(error);
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      cacheTime: 5 * 60 * 1000,
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      retry: 0,
+      keepPreviousData: false,
+      onError,
+    },
+    mutations: {
+      retry: 0,
+      onError,
+    },
+  },
+});
+
+export default queryClient;
